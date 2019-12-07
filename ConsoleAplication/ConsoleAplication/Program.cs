a61b0e05364342d82f8049607d800d8c6d4ab796
Cesar Verano <cesar.verano@Ipsos.com>
2019-12-06 19:55:20 -0500

    Initial commit

---
+++ ConsoleAplication/ConsoleAplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
